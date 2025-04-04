f80f91c99b2af04ab13641761474cffd4a514c4c
Gabriel Piske <156837794+gabrielpiske@users.noreply.github.com>
2025-04-03 21:02:59 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -86,42 +86,42 @@ export const fios = [
     { 
         tipo: "EPR/XLPE 1KV MULTIFILAR 3 VIAS", 
         tamanhos: [
-            { nome: "Cabo 1,5mm²", area: 3 * Math.PI * Math.pow(8.43, 2) /4 },
-            { nome: "Cabo 2,5mm²", area: 3 * Math.PI * Math.pow(9.53, 2) /4 },
-            { nome: "Cabo 4mm²", area: 3 * Math.PI * Math.pow(10.7, 2) /4 },
-            { nome: "Cabo 6mm²", area: 3 * Math.PI * Math.pow(11.9, 2) /4 },
-            { nome: "Cabo 10mm²", area: 3 * Math.PI * Math.pow(13.6, 2) /4 },
-            { nome: "Cabo 16mm²", area: 3 * Math.PI * Math.pow(16, 2) /4 },
-            { nome: "Cabo 25mm²", area: 3 * Math.PI * Math.pow(19.6, 2) /4 },
-            { nome: "Cabo 35mm²", area: 3 * Math.PI * Math.pow(22.4, 2) /4 },
-            { nome: "Cabo 50mm²", area: 3 * Math.PI * Math.pow(25.6, 2) /4 },
-            { nome: "Cabo 70mm²", area: 3 * Math.PI * Math.pow(29, 2) /4 },
-            { nome: "Cabo 95mm²", area: 3 * Math.PI * Math.pow(32.9, 2) /4 },
-            { nome: "Cabo 120mm²", area: 3 * Math.PI * Math.pow(38.8, 2) /4 },
-            { nome: "Cabo 150mm²", area: 3 * Math.PI * Math.pow(42.2, 2) /4 },
-            { nome: "Cabo 185mm²", area: 3 * Math.PI * Math.pow(47.1, 2) /4 },
-            { nome: "Cabo 240mm²", area: 3 * Math.PI * Math.pow(53.4, 2) / 4}
+            { nome: "Cabo 1,5mm²", area: Math.PI * Math.pow(8.43, 2) /4 },
+            { nome: "Cabo 2,5mm²", area: Math.PI * Math.pow(9.53, 2) /4 },
+            { nome: "Cabo 4mm²", area: Math.PI * Math.pow(10.7, 2) /4 },
+            { nome: "Cabo 6mm²", area: Math.PI * Math.pow(11.9, 2) /4 },
+            { nome: "Cabo 10mm²", area: Math.PI * Math.pow(13.6, 2) /4 },
+            { nome: "Cabo 16mm²", area: Math.PI * Math.pow(16, 2) /4 },
+            { nome: "Cabo 25mm²", area: Math.PI * Math.pow(19.6, 2) /4 },
+            { nome: "Cabo 35mm²", area: Math.PI * Math.pow(22.4, 2) /4 },
+            { nome: "Cabo 50mm²", area: Math.PI * Math.pow(25.6, 2) /4 },
+            { nome: "Cabo 70mm²", area: Math.PI * Math.pow(29, 2) /4 },
+            { nome: "Cabo 95mm²", area: Math.PI * Math.pow(32.9, 2) /4 },
+            { nome: "Cabo 120mm²", area: Math.PI * Math.pow(38.8, 2) /4 },
+            { nome: "Cabo 150mm²", area: Math.PI * Math.pow(42.2, 2) /4 },
+            { nome: "Cabo 185mm²", area: Math.PI * Math.pow(47.1, 2) /4 },
+            { nome: "Cabo 240mm²", area: Math.PI * Math.pow(53.4, 2) / 4}
         ]
     },
 
     {
         tipo: "EPR/XLPE 1KV MULTIFILAR 4 VIAS",
         tamanhos: [
-            { nome: "Cabo 1.5mm²", area: 3 * Math.PI * Math.pow(9.4, 2) / 4},
-            { nome: "Cabo 2.5mm²", area: 3 * Math.PI * Math.pow(10.4, 2) / 4},
-            { nome: "Cabo 4mm²", area: 3 * Math.PI * Math.pow(11.8, 2) / 4},
-            { nome: "Cabo 6mm²", area: 3 * Math.PI * Math.pow(13.3, 2) / 4},
-            { nome: "Cabo 10mm²", area: 3 * Math.PI * Math.pow(15, 2) / 4},
-            { nome: "Cabo 16mm²", area: 3 * Math.PI * Math.pow(17.6, 2) / 4},
-            { nome: "Cabo 25mm²", area: 3 * Math.PI * Math.pow(22, 2) / 4},
-            { nome: "Cabo 35mm²", area: 3 * Math.PI * Math.pow(24.9, 2) / 4},
-            { nome: "Cabo 50mm²", area: 3 * Math.PI * Math.pow(28.5, 2) / 4},
-            { nome: "Cabo 70mm²", area: 3 * Math.PI * Math.pow(32.3, 2) / 4},
-            { nome: "Cabo 95mm²", area: 3 * Math.PI * Math.pow(36.6, 2) / 4},
-            { nome: "Cabo 120mm²", area: 3 * Math.PI * Math.pow(43.1, 2) / 4},
-            { nome: "Cabo 150mm²", area: 3 * Math.PI * Math.pow(46.9, 2) / 4},
-            { nome: "Cabo 185mm²", area: 3 * Math.PI * Math.pow(52.4, 2) / 4},
-            { nome: "Cabo 240mm²", area: 3 * Math.PI * Math.pow(59.3, 2) / 4}
+            { nome: "Cabo 1.5mm²", area: Math.PI * Math.pow(9.4, 2) / 4},
+            { nome: "Cabo 2.5mm²", area: Math.PI * Math.pow(10.4, 2) / 4},
+            { nome: "Cabo 4mm²", area: Math.PI * Math.pow(11.8, 2) / 4},
+            { nome: "Cabo 6mm²", area: Math.PI * Math.pow(13.3, 2) / 4},
+            { nome: "Cabo 10mm²", area: Math.PI * Math.pow(15, 2) / 4},
+            { nome: "Cabo 16mm²", area: Math.PI * Math.pow(17.6, 2) / 4},
+            { nome: "Cabo 25mm²", area: Math.PI * Math.pow(22, 2) / 4},
+            { nome: "Cabo 35mm²", area: Math.PI * Math.pow(24.9, 2) / 4},
+            { nome: "Cabo 50mm²", area: Math.PI * Math.pow(28.5, 2) / 4},
+            { nome: "Cabo 70mm²", area: Math.PI * Math.pow(32.3, 2) / 4},
+            { nome: "Cabo 95mm²", area: Math.PI * Math.pow(36.6, 2) / 4},
+            { nome: "Cabo 120mm²", area: Math.PI * Math.pow(43.1, 2) / 4},
+            { nome: "Cabo 150mm²", area: Math.PI * Math.pow(46.9, 2) / 4},
+            { nome: "Cabo 185mm²", area: Math.PI * Math.pow(52.4, 2) / 4},
+            { nome: "Cabo 240mm²", area: Math.PI * Math.pow(59.3, 2) / 4}
         ]
     }
 ];
